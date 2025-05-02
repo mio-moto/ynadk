4f2442f3a9b2dd73650e6928d0c5f7fa05171962
Mio Moto <mio-moto@users.noreply.github.com>
2025-05-02 17:57:47 +0200

    fix: linter, some hydration mistakes

--- src/features/UserGuide.tsx
+++ src/features/UserGuide.tsx
@@ -1,5 +1,5 @@
 import { css, cx } from '@linaria/core'
-import { type FC, useCallback, useEffect, useReducer, useRef, useState } from 'react'
+import { type FC, useCallback, useEffect, useRef, useState } from 'react'
 import { style } from '../app/style/style'
 import { themeColors } from '../app/style/themeColors'
 import { Button } from '../components/Button'
@@ -126,13 +126,13 @@ export const UserGuide: FC<{ forceOpen?: boolean; onClose?: () => void }> = ({ f
         x
       </div>
       <div className="content">
-        <p>
+        <div>
           This is <code className="title">You Need A Drum Kit</code>, a sample compiler focused for the M8 Tracker. The primary purpose is to combine
           a sound font, a drum kit or sound kit into a single <code>wav</code> file with the cue points being set for you, easing the process of
           creating a sliced sample that can be used with devices that support it.
-        </p>
+        </div>
 
-        <p>
+        <div>
           <h3>Ok, and now?</h3>
           The process is quick and easy:
           <ul>
@@ -149,16 +149,16 @@ export const UserGuide: FC<{ forceOpen?: boolean; onClose?: () => void }> = ({ f
           </ul>
           That's it, you can now play the samples by the corresponding note you assigned that sample as slot to. Bonus points if you're using the
           arpeggiator to randomize your drum patterns.
-        </p>
+        </div>
 
         <div>
           <h3>What's stride?</h3>
           You can configure the <span className="emphasis">layout</span> of your kit on the right kit menu. This allows you to assign the same kind of
           instrument, for example:
-          <p>
+          <div>
             <code>Kick x2</code> <code>Hi-Hat x2</code> <code>Ride x2</code> <code>Tom Tom x2</code> <code>Conga</code> <code>Clap x2</code>{' '}
             <code>Shake</code> (total of 12 Samples per kit)
-          </p>
+          </div>
           This allows you to configure that kit and then assign your samples to the pre-configured slots. If you use the M8 Tracker, you can then
           create a chain with your drum loop and use the <code>TSP</code> inside a chain to swap out drum kit with the same beat structure.
           <div className="gallery">
@@ -167,7 +167,7 @@ export const UserGuide: FC<{ forceOpen?: boolean; onClose?: () => void }> = ({ f
           </div>
         </div>
 
-        <p>
+        <div>
           <h3>Help? Problems? Code? License?!</h3>
           <div>
             Why yes, glad you asked. The code is{' '}
@@ -191,7 +191,7 @@ export const UserGuide: FC<{ forceOpen?: boolean; onClose?: () => void }> = ({ f
             , the license of the compiled <code>wav</code> file is inherited by the licenses of the samples used and this project sets no limitations
             on that.
           </div>
-        </p>
+        </div>
       </div>
 
       <Button onClick={closeUserGuide} className="button">
